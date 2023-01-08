d78ec6a64ac068bb29bc6642a512bb8d5d03e450
Aaron Lipman <alipman88@gmail.com>
2023-01-08 14:22:13 -0500

    replace app content with static webview

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+import { useAssets } from 'expo-asset';
+import { useState } from 'react';
+import { readAsStringAsync } from 'expo-file-system';
 
 export default function App() {
+  const [index, indexLoadingError] = useAssets(
+    require('./index.html')
+  );
+
+  const [html, setHtml] = useState('');
+
+  if (index) {
+    readAsStringAsync(index[0].localUri).then((data) => {
+      setHtml(data);
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#333' }}>
+      <WebView
+        source={{ html }}
+      />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
